23b6e4912f1162a5d13b77dbe7888891978a2691
jm.sun <jm.sun@shunwang.com>
2021-04-30 14:33:52 +0800

    w

--- client/zmq_discovery.go
+++ client/zmq_discovery.go
@@ -54,7 +54,7 @@ func NewZmqDiscoveryStore(basePath string, kv store.Store) (ServiceDiscovery, er
 	}
 	pairs := make([]*KVPair, 0, len(ps))
 	for _, p := range ps {
-		pair := &KVPair{Key: p.Key, Value: string(p.Value)}
+		pair := &KVPair{Key: string(p.Value), Value: string(p.Value)}
 		if d.filter != nil && !d.filter(pair) {
 			continue
 		}
@@ -179,7 +179,7 @@ func (d *ZmqDiscovery) watch() {
 					continue
 				}
 				for _, p := range ps {
-					pair := &KVPair{Key: p.Key, Value: string(p.Value)}
+					pair := &KVPair{Key: string(p.Value), Value: string(p.Value)}
 					if d.filter != nil && !d.filter(pair) {
 						continue
 					}
